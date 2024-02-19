31364350cfe9d2f3e80ba6bfa0a32650623ba388
Saurav Mallik <sauravmallik786@example.com>
2024-02-19 17:46:28 +0530

    ad

--- video-player-app/src/Components/Feed.jsx
+++ video-player-app/src/Components/Feed.jsx
@@ -12,26 +12,24 @@ const Feed = () => {
   },[])
 
   return (
-    <div className='flex flex-row h-[calc(100%-56px)]'>
-      <LeftNav />
-      <div className='grow w-[calc[100%-240px]] h-full overflow-y-auto bg-black'>
-        <div className='grid grid-cols-1 md:grid-col-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-5'>
-          {!loading && searchResults.map((ele) => {
-            if(ele.type !== "video") return false;
-            return (
-                <VideoCard 
-                key={ele?.video?.videoId}
-                video={ele?.video}
-                 />
-            )
-          }
-          
-          )}
+    <div className="flex flex-row h-[calc(100%-56px)]">
+    <LeftNav />
+    <div className="grow w-[calc(100%-240px)] h-full overflow-y-auto bg-black">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-5">
+            {!loading &&
+                searchResults.map((item) => {
+                    if (item.type !== "video") return false;
+                    return (
+                        <VideoCard
+                            key={item?.video?.videoId}
+                            video={item?.video}
+                        />
+                    );
+                })}
         </div>
-      </div>
-      
     </div>
+</div>
   )
 }
 
-export default Feed
+export default Feed;
