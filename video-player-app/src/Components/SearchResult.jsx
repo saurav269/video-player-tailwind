8a7b37a5771545294dcdb88defd72c6d5c61c5ff
Saurav Mallik <sauravmallik786@example.com>
2024-02-21 14:48:46 +0530

    renewed

--- video-player-app/src/Components/SearchResult.jsx
+++ video-player-app/src/Components/SearchResult.jsx
@@ -6,38 +6,42 @@ import LeftNav from './LeftNav';
 import SearchResultVideoCard from './SearchResultVideoCard';
 
 const SearchResult = () => {
-  const [result, setResult] = useState([]);
-  const {searchQuery} = useParams();
-  const {setLoading} = useContext(Context); 
+  const [result, setResult] = useState();
+  const { searchQuery } = useParams();
+  const { setLoading } = useContext(Context);
 
   useEffect(() => {
-    document.getElementById("root").classList.remove("custom-h")
-    fetchSesarchResult()
-  },[searchQuery])
+      document.getElementById("root").classList.remove("custom-h");
+      fetchSearchResults();
+  }, [searchQuery]);
 
-  const fetchSesarchResult=()=>{
-    setLoading(true);
-    fetchDataApi(`/search/?q=${searchQuery}`).then((res) =>{
-      setResult(res?.conents)
-      setLoading(false)
-    })
-  }
+  const fetchSearchResults = () => {
+      setLoading(true);
+      fetchDataApi(`search/?q=${searchQuery}`).then((res) => {
+          setResult(res?.contents);
+          setLoading(false);
+      });
+  };
+
   return (
-    <div className='flex flex-row h-[calc(100%-56px)]'>
-      <LeftNav />
-      <div className="grow w-[calc(100%-240px)] h-full overflow-y-auto bg-black">
-        <div className='grid grid-cols-1 gap-2 p-5'>
-          {result?.map((item) => {
-            if(item?.type !== "video") return false
-            let video=item?.video
-            return (
-              <SearchResultVideoCard key={video?.videoId} video={video} />
-            )
-          })}
-        </div>
+      <div className="flex flex-row h-[calc(100%-56px)]">
+          <LeftNav />
+          <div className="grow w-[calc(100%-240px)] h-full overflow-y-auto bg-black">
+              <div className="grid grid-cols-1 gap-2 p-5">
+                  {result?.map((item) => {
+                      if (item?.type !== "video") return false;
+                      let video = item.video;
+                      return (
+                          <SearchResultVideoCard
+                              key={video.videoId}
+                              video={video}
+                          />
+                      );
+                  })}
+              </div>
+          </div>
       </div>
-    </div>
-  )
+  );
 }
 
 export default SearchResult
